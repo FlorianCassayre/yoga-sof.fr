38cfa2493c4af5ada3c7586e0c5ddf35bfd5957b
Florian Cassayre <florian.cassayre@gmail.com>
2023-01-19 22:53:15 +0100

    Collapsible grid feature

--- src/components/grid/AsyncGrid.tsx
+++ src/components/grid/AsyncGrid.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid, gridClasses, GridColDef } from '@mui/x-data-grid';
-import { inferHandlerInput } from '@trpc/server';
-import { AppRouter } from '../../server/controllers';
-import { trpc } from '../../common/trpc';
-import { Card, useTheme } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Card } from '@mui/material';
 import { GridRowIdGetter, GridValidRowModel } from '@mui/x-data-grid/models/gridRows';
-import { Procedure, ProcedureParams } from '@trpc/server/src/core/procedure';
-import { AnyRootConfig } from '@trpc/server/src/core/internals/config';
 import { inferProcedureInput } from '@trpc/server/dist/core/types';
 import { DecorateProcedure } from '@trpc/react-query/shared';
 import { GridColumns } from '@mui/x-data-grid/models/colDef/gridColDef';
+import { ExpandMore } from '@mui/icons-material';
+import { AnyRootConfig, Procedure, ProcedureParams } from '@trpc/server';
 
 type ProcedureQueryArray<T> = Procedure<'query', ProcedureParams<AnyRootConfig, unknown, unknown, unknown, unknown, T[], unknown>>;
 
@@ -20,44 +17,89 @@ interface AsyncGridProps<T, TProcedure extends ProcedureQueryArray<T>> {
   getRowId?: GridRowIdGetter;
   getRowClassName?: (row: any) => any;
   initialSort: { field: string, sort: 'asc' | 'desc' };
+  collapsible?: boolean;
+  collapsedSummary?: React.ReactNode;
 }
 
 const rowsPerPageOptions = [10];
 
-export const AsyncGrid = <RowModel extends GridValidRowModel, Columns extends GridColumns<RowModel>, TProcedure extends ProcedureQueryArray<RowModel>>({ columns, procedure, input, getRowId, getRowClassName, initialSort }: AsyncGridProps<RowModel, TProcedure>): JSX.Element => {
+export const AsyncGrid = <RowModel extends GridValidRowModel, Columns extends GridColumns<RowModel>, TProcedure extends ProcedureQueryArray<RowModel>>({ columns, procedure, input, getRowId, getRowClassName, initialSort, collapsible, collapsedSummary }: AsyncGridProps<RowModel, TProcedure>): JSX.Element => {
   //const theme = useTheme();
-  const { data, isLoading, isError } = procedure.useQuery(input);
+  const [enabled, setEnabled] = useState(!collapsible);
+  const { data, isLoading, isError } = procedure.useQuery(input, { enabled });
 
-  return (
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggleAccordion = () => {
+    setEnabled(true);
+    setExpanded(!expanded);
+  };
+
+  const renderDataGrid = () => (
+    <DataGrid
+      rows={data ?? []}
+      columns={columns}
+      getRowId={getRowId}
+      autoHeight
+      pageSize={rowsPerPageOptions[0]}
+      rowsPerPageOptions={rowsPerPageOptions}
+      loading={isLoading}
+      getRowClassName={getRowClassName}
+      sortingOrder={['asc', 'desc']}
+      initialState={{ sorting: { sortModel: [initialSort] } }}
+      disableColumnMenu
+      disableColumnSelector
+      disableSelectionOnClick
+      sx={{
+        // Disable the annoying focus outline
+        [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]: {
+          outline: "none"
+        },
+        [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]: {
+          outline: "none"
+        },
+        // Hide the column separators
+        '& .MuiDataGrid-columnSeparator': {
+          display: 'none',
+        },
+        // Accordion-only
+        ...(collapsible ? {
+          // Fit the accordion
+          m: -0.1,
+          mt: 0,
+          // No rounded border with accordion
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+        } : {}),
+      }}
+    />
+  );
+
+  return collapsible ? (
+    <Accordion
+      expanded={expanded}
+      onChange={handleToggleAccordion}
+      disableGutters
+      elevation={0}
+      variant="outlined"
+      sx={{
+        width: '100%',
+        '&:before': { // Remove the black line above the accordion (?)
+          display: 'none',
+        },
+        borderRadius: 1,
+      }}
+    >
+      <AccordionSummary expandIcon={<ExpandMore />}>
+        {collapsedSummary}
+      </AccordionSummary>
+      <AccordionDetails sx={{ p: 0 }}>
+        {renderDataGrid()}
+      </AccordionDetails>
+    </Accordion>
+  ) : (
     <Card elevation={0} sx={{ width: '100%' }}>
-      <DataGrid
-        rows={data ?? []}
-        columns={columns}
-        getRowId={getRowId}
-        autoHeight
-        pageSize={rowsPerPageOptions[0]}
-        rowsPerPageOptions={rowsPerPageOptions}
-        loading={isLoading}
-        getRowClassName={getRowClassName}
-        sortingOrder={['asc', 'desc']}
-        initialState={{ sorting: { sortModel: [initialSort] } }}
-        disableColumnMenu
-        disableColumnSelector
-        disableSelectionOnClick
-        sx={{
-          // Disable the annoying focus outline
-          [`& .${gridClasses.cell}:focus, & .${gridClasses.cell}:focus-within`]: {
-            outline: "none"
-          },
-          [`& .${gridClasses.columnHeader}:focus, & .${gridClasses.columnHeader}:focus-within`]: {
-            outline: "none"
-          },
-          // Hide the column separators
-          '& .MuiDataGrid-columnSeparator': {
-            display: 'none',
-          },
-        }}
-      />
+      {renderDataGrid()}
     </Card>
   );
 };
