6799c0984e15442fbff94e13e979bf3758dacb7d
Florian Cassayre <florian.cassayre@gmail.com>
2023-01-22 11:28:53 +0100

    Display the membership status on the grid

--- src/server/controllers/types.ts
+++ src/server/controllers/types.ts
@@ -1,6 +1,8 @@
 import { Serialize } from '@trpc/server/dist/shared/internal/serialize';
 import { Procedure, ProcedureParams } from '@trpc/server/src/core/procedure';
 import { AnyRootConfig } from '@trpc/server/src/core/internals/config';
+import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
+import { AppRouter } from './router';
 
 type NonJsonPrimitive = undefined | Function | symbol;
 export type ConvertInput<T> = T | (T extends NonJsonPrimitive ? null : Serialize<T>);
@@ -8,3 +10,6 @@ export type ConvertInput<T> = T | (T extends NonJsonPrimitive ? null : Serialize
 export type ProcedureQueryArray<TOutput, TInput = undefined> = Procedure<'query', ProcedureParams<AnyRootConfig, unknown, TInput, unknown, unknown, TOutput[], unknown>>;
 
 export type InputIdentifier = { id: number | string };
+
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
