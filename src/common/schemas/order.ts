662c8bf26641800cc35c06da48d122ec2c0f095d
Florian Cassayre <florian.cassayre@gmail.com>
2023-03-22 21:38:07 +0100

    Weak validation

--- src/common/schemas/order.ts
+++ src/common/schemas/order.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 import { MembershipType, TransactionType } from '@prisma/client';
 
-const checkUniqueIds = [(array: number[]) => new Set(array).size === array.length, { message: `Les éléments doivent être distincts` }] as const;
-const checkUniqueRefIds = [(array: { id: number }[]) => new Set(array.map(({ id }) => id)).size === array.length, { message: `Les éléments doivent être distincts` }] as const;
+const checkUniqueFor = <T>(idGetter: (e: T) => number, message: string = `Les éléments doivent être distincts`) =>
+  [
+    (array: T[]) => new Set(array.map(e => idGetter(e))).size === array.length,
+    { message }
+  ] as const;
+
+const checkUniqueIds = checkUniqueFor((id: number) => id);
+const checkUniqueRefIds = checkUniqueFor(({ id }: { id: number }) => id);
 
 export const orderCreateSchema = z.strictObject({
   user: z.object({
@@ -19,29 +25,32 @@ export const orderCreateSchema = z.strictObject({
     })).optional(),
     existingCoupons: z.array(z.object({
       id: z.number().int().min(0),
-    })).min(1).optional(),
+    })).min(1).refine(...checkUniqueRefIds).optional(),
     newMemberships: z.array(z.strictObject({
       membershipModelId: z.nativeEnum(MembershipType),
       year: z.number().int().min(2021).max(2100),
     })).optional(),
-    existingMemberships: z.array(z.object({
-      id: z.number().int().min(0),
-    })).min(1).optional(),
+    existingMembershipIds: z.array(
+      z.number().int().min(0)
+    ).min(1).refine(...checkUniqueIds).optional(),
   }),
   billing: z.strictObject({
     newCoupons: z.array(z.strictObject({
       newCouponIndex: z.number().int().min(0),
-      courseRegistrationIds: z.array(z.number().int().min(0)).min(1),
-    })).min(1).optional(),
+      courseRegistrationIds: z.array(z.number().int().min(0)).min(1).refine(...checkUniqueIds),
+    })).min(1).refine(...checkUniqueFor((e: { newCouponIndex: number }) => e.newCouponIndex)).optional(),
     existingCoupons: z.array(z.strictObject({
       couponId: z.number().int().min(0),
-      courseRegistrationIds: z.array(z.number().int().min(0)).min(1),
-    })).min(1).optional(),
+      courseRegistrationIds: z.array(z.number().int().min(0)).min(1).refine(...checkUniqueIds),
+    })).min(1).refine(...checkUniqueFor((e: { couponId: number }) => e.couponId)).optional(),
     trialCourseRegistrationId: z.number().int().min(0).optional(),
     replacementCourseRegistrations: z.array(z.strictObject({
-      oldCourseRegistrationId: z.number().int().min(0),
-      replacedByCourseRegistrationId: z.number().int().min(0),
-    })).optional(),
+      fromCourseRegistrationId: z.number().int().min(0),
+      toCourseRegistrationId: z.number().int().min(0),
+    }))
+      .refine(...checkUniqueFor((e: { fromCourseRegistrationId: number }) => e.fromCourseRegistrationId))
+      .refine(...checkUniqueFor((e: { toCourseRegistrationId: number }) => e.toCourseRegistrationId))
+      .optional(),
     transactionId: z.number().int().min(0).optional(),
     newPayment: z.strictObject({
       amount: z.number().int().min(1),
