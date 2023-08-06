5f046345cdd5bfc1a31a2094e304d6e0ac25cb3c
Yoga-Sof <8232047+Sofalsace@users.noreply.github.com>
2023-08-06 14:58:07 +0200

    Update courses.tsx

--- contents/courses.tsx
+++ contents/courses.tsx
@@ -12,7 +12,7 @@ export const coursesExplicit: Record<CourseType, CourseDataExplicit> = {
     level: `Tous niveaux`,
     group: `4-5 personnes`,
     duration: `1h15`,
-    price: `360 € les 30 séances ou 130 € les 10 séances ou 15 € / scéance + cotisation association 15 € / personne ou 20 € / famille`,
+    price: `360 € les 30 séances ou 130 € les 10 séances ou 15 € / scéance + cotisation annuelle association 15 € / personne ou 20 € / famille`,
     location: <>À mon domicile au <Link href={LocationHome.googleUrl} target="_blank" rel="noreferrer nofollow">8 rue des moissonneurs, 68220 Hésingue</Link>. Je peux également venir chez vous si vous constituez un petit groupe, n'hésitez pas à me contacter pour cela.</>,
     stuff: `Tapis de Yoga et une couverture, portez une tenue confortable. Vous pourrez emprunter sur place des blocs, sangles, élastiques, coussins, bolster, balles et ballons.`,
     registration: `Inscription en ligne depuis le site, ou bien me contacter`,
